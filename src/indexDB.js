c795380d927da557ac9685f69feb82f778545bad
NinaD <nina.dong4764@gmail.com>
2026-03-06 20:08:48 -0800

    doc: wrote a few code for opening store and working with indexDB.

--- src/indexDB.js
+++ src/indexDB.js
@@ -1,5 +1,34 @@
-function addSnapshot(snapshot) {
+function openDB() {
+    return new Promise((resolve, reject) => {
+        const DBRequest = indexedDB.open("ao3Data", 1);
+        DBRequest.onupgradeneeded = function(event) {
+            const db = event.target.result;
+            //creates a table named snapshots
+            db.createObjectStore("snapshots", {
+                keyPath: "snapshotId"
+            })
+            db.createObjectStore("metadata", {})
+        } 
+        DBRequest.onsuccess = function (event) {
+            const db = event.target.result;
+            resolve(db);
+        }
+        DBRequest.onerror = function (event) {
+            reject(event.target.error);
+        }
+    });
+}
 
+async function getStore(storeName) {
+    const db = await openDB();
+    transaction = db.transaction(storeName, "readwrite");
+    store = transaction.objectStore(storeName);
+    return store;
+}
+
+async function addSnapshot(snapshot) {
+    snapshotStore = await getStore("snapshots");
+    snapshotStore.add(snapshot);
 }
 
 function findSnapshot(snapshotId) {
