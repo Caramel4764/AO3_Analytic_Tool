cf9f115e556c6d00fc1aced6e12f18a181bb90fc
NinaD <nina.dong4764@gmail.com>
2026-03-07 00:33:33 -0800

    doc|feat: create functions for working with indexDB. Add scraperController which combine scraping the ao3 website page specifiec by user with storing it in indexDB. Fix snapshop not having workID.

--- src/indexDB.js
+++ src/indexDB.js
@@ -1,3 +1,16 @@
+
+function promiseRequest(request, customSuccessMessage="", customErrorMessage="") {
+    return new Promise((resolve, reject)=> {
+        request.onsuccess = () => {
+            if (customSuccessMessage != "") {
+                console.log(`Success: ${customSuccessMessage}`);
+            }
+            resolve(request.result);
+        };
+        request.onerror = () => reject(new Error(`${customErrorMessage}: ${request.error}`));
+    })
+}
+
 function openDB() {
     return new Promise((resolve, reject) => {
         const DBRequest = indexedDB.open("ao3Data", 1);
@@ -21,37 +34,60 @@ function openDB() {
 
 async function getStore(storeName) {
     const db = await openDB();
-    transaction = db.transaction(storeName, "readwrite");
-    store = transaction.objectStore(storeName);
+    let transaction = db.transaction(storeName, "readwrite");
+    let store = transaction.objectStore(storeName);
     return store;
 }
 
 async function addSnapshot(snapshot) {
-    snapshotStore = await getStore("snapshots");
-    snapshotStore.add(snapshot);
+    let snapshotStore = await getStore("snapshots");
+    let request = snapshotStore.put(snapshot);
+    return promiseRequest(request, "Successful added snapshot", "Could not add snapshot");
 }
 
-function findSnapshot(snapshotId) {
-    
+async function getSnapshot(snapshotId) {
+    let snapshotStore = await getStore("snapshots");
+    let request = snapshotStore.get(snapshotId);
+    return promiseRequest(request, "FoundSnapShot", "Could not find snapshot");
 }
 
-function removeSnapshot(snapshotId) {
-
+async function getAllSnapshots() {
+    let snapshotStore = await getStore("snapshots");
+    let request = snapshotStore.getAll();
+    return promiseRequest(request, "Fetched all snapshot", "Fetching all snapshots failed")
 }
 
-function findWork(workID) {
-
+async function removeSnapshot(snapshotId) {
+    let snapshotStore = await getStore("snapshots");
+    let request = snapshotStore.delete(snapshotId);
+    return promiseRequest(request, "Deleted", "Could not delete because snapshot Id not found");
 }
 
-function removeWork(workID) {
+async function clearSnapshot() {
+    let snapshotStore = await getStore("snapshots");
+    let request = snapshotStore.clear();
+    return promiseRequest(request, "All snapshot cleared", "Could not delete all");
+}
 
+async function findWork(workID) {
+    let metadataStore = await getStore("snapshots");
+    let request = metadataStore.get(workID);
+    return promiseRequest(request, "Found Work", "Could not find snapshot");
+}
+
+async function removeWork(workID) {
+    let metadataStore = await getStore("snapshots");
+    let request = metadataStore.delete(workID);
+    return promiseRequest(request, "Deleted Work", "Could not delete because work Id not found");
 }
 let indexDB = {
     addSnapshot,
-    findSnapshot,
+    getSnapshot,
     removeSnapshot,
     findWork,
-    removeWork
+    removeWork,
+    getAllSnapshots,
+    clearSnapshot
 }
 
 export default indexDB;
--- src/scraperController.js
+++ src/scraperController.js
@@ -1,13 +1,17 @@
 import HTMLParserUtil from "./HTMLParserUtil.js";
 import Ao3WorkDom from "./Ao3WorkDom.js";
+import indexDB from "./indexDB.js";
 
 async function scrapeWebsite (link) {
     //fetch information
     let HTMLString = await HTMLParserUtil.fetchHTML(link)
     let HTMLDom = HTMLParserUtil.stringHTMLToDom(HTMLString);
-    newAo3WorkDom = new Ao3WorkDom(HTMLDom, link);
+    let newAo3WorkDom = new Ao3WorkDom(HTMLDom, link);
     //store info
-    
+    //indexDB.clearSnapshot();
+    indexDB.addSnapshot(newAo3WorkDom.getSnapshot());
+    console.log("FoundSnapShot: ", indexDB.getSnapshot(newAo3WorkDom.getSnapshotId()));
+    console.log("AllSnapshots: ", indexDB.getAllSnapshots());
 }
 
 let scraperController = {
