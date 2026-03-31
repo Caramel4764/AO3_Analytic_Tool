e26e0dc6b6207b51353adee1b18b80ca95165f77
Caramel4764 <nina.dong4764@gmail.com>
2026-03-30 20:39:57 -0700

    doc: finish up style for popup and add an alternative message saying there is tracked work if metadata's length is 0.

--- src/popup/CWork.ts
+++ src/popup/CWork.ts
@@ -36,11 +36,9 @@ class CWork {
       </div>
       <div class="flex actionsDiv" id=actions${this.metadata.workId}>
         <button title="Show stats" id="action_btn_view_${this.metadata.workId}" class="action_btn btn_with_icon action_btn_view">
-          <svg xmlns="http://www.w3.org/2000/svg"  width=${icon_size} height=${icon_size} fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
-            <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+          <svg xmlns="http://www.w3.org/2000/svg" width=${icon_size} height=${icon_size} fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M3 13.125C3 12.504 3.504 12 4.125 12h2.25c.621 0 1.125.504 1.125 1.125v6.75C7.5 20.496 6.996 21 6.375 21h-2.25A1.125 1.125 0 0 1 3 19.875v-6.75ZM9.75 8.625c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125v11.25c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 0 1-1.125-1.125V8.625ZM16.5 4.125c0-.621.504-1.125 1.125-1.125h2.25C20.496 3 21 3.504 21 4.125v15.75c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 0 1-1.125-1.125V4.125Z" />
           </svg>
-
         </button>
         <button title="Pause tracking" id="action_btn_pause_${this.metadata.workId}" class="btn_with_icon action_btn action_btn_pause">
           <svg xmlns="http://www.w3.org/2000/svg" width=${icon_size} height=${icon_size} fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
